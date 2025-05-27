eeddd3e85591851f38100b336ad15444f2dbbb0b
DanyilMak <danyil.maksymchuk@oa.edu.ua>
2025-05-27 12:29:21 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
+  <v-app>
+    <v-app-bar app fixed>
+      <v-app-bar-nav-icon @click="drawer = !drawer">
+        <v-icon>mdi-menu</v-icon>
+      </v-app-bar-nav-icon>
+      <v-app-bar-title>My Project</v-app-bar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list>
+        <v-list-item v-for="item in items" :key="item.title" link :to="item.to" :title="item.title">
+          <template v-slot:prepend>
+            <v-icon>{{ item.icon }}</v-icon>
+          </template>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const drawer = ref(null)
+const items = ref([
+  { title: 'List', to: '/', icon: 'mdi-format-list-checks' },
+  { title: 'Group', to: '/group', icon: 'mdi-account-group' },
+  { title: 'About', to: '/about', icon: 'mdi-help-box' },
+])
+const right = ref(false)
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about pa-6">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+}
+</style>
